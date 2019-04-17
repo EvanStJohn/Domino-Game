d1fa66e53197386c499318845183cc76295290e9
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-04-17 10:53:19 -0700

    Update CBoard.java

--- src/dominos/CBoard.java
+++ src/dominos/CBoard.java
@@ -2,5 +2,37 @@ package dominos;
 
 
 public class CBoard {
+    int front;
+    int end;
     
+    public CBoard() {
+        front = 0;
+        end = 0;
+    }
+    
+    public void playFirstTile(CDomino tile) {
+        front = tile.left;
+        end = tile.right;
+    }
+    
+    public void playTile(CDomino tile, int location) {
+        //0 is end, 1 is front
+        //Check if play is playable in CGame
+        if (location == 0) {
+            if (tile.left == end) {
+                end = tile.right;
+            }
+            else {
+                end = tile.left;
+            }
+        }
+        if (location == 1) {
+            if (tile.left == front) {
+                front = tile.right;
+            }
+            else {
+                front = tile.left;
+            }
+        }
+    }   
 }
