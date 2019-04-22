cf76b4e9862156e246d3b29bb5ae5c63596cf953
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-04-21 22:39:14 -0700

    Update CBoard.java

--- src/dominos/CBoard.java
+++ src/dominos/CBoard.java
@@ -5,18 +5,22 @@ public class CBoard {
     private int front;
     private int end;
     
+    //board constructor. consists of front and end values of tile chain
     public CBoard() {
         front = 0;
         end = 0;
     }
     
+    //allows for the first play of a tile
     public void playFirstTile(CDomino tile) {
         front = tile.getLeft();
         end = tile.getRight();
     }
     
+    //plays tile at given location
     public void playTile(CDomino tile, int location) {
         //0 is end, 1 is front
+        //Check if play is playable in CGame
         if (location == 0) {
             if (tile.getLeft() == end) {
                 end = tile.getRight();
@@ -35,10 +39,12 @@ public class CBoard {
         }
     }
     
+    //returns front
     public int getFront() {
     	return front;
     }
     
+    //returns end
     public int getEnd() {
     	return end;
     }
