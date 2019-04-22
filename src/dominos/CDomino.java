63ceda771dbc913cf9d0a9e57eba3d88876d1384
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-04-21 22:37:48 -0700

    Update CDomino.java

--- src/dominos/CDomino.java
+++ src/dominos/CDomino.java
@@ -5,27 +5,32 @@ public class CDomino {
     
     private int left, right;
     
+    //constructor of domino. contains value for left and right
     public CDomino(int l, int r)
     {
         left = l;
         right = r;
     }
     
+    //returns left
     public int getLeft()
     {
         return left;
     }
     
+    //returns right
     public int getRight()
     {
         return right;
     }
     
+    //returns total value of domino
     public int getTotal()
     {
         return left + right;
     }
     
+    //flips values of domino
     public void flip()
     {
         int num = right;
@@ -33,17 +38,19 @@ public class CDomino {
         left = num;
     }
     
+    //horizontally prints domino
     public void printH()
     {
         System.out.print("[" + left + " | " + right + "]");
     }
     
+  //vertically prints domino
     public void printV()
     {
         System.out.println("---");
-	System.out.println(" " + left);
-	System.out.println(" -");
-	System.out.println(" " + right);
-	System.out.println("---");
+        System.out.println(" " + left);
+        System.out.println(" -");
+        System.out.println(" " + right);
+        System.out.println("---");
     }
 }
