97bea09c1ca3d4e5ecaecea438223fa1ba3d5a78
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-04-21 22:34:54 -0700

    Update CPlayer.java

--- src/dominos/CPlayer.java
+++ src/dominos/CPlayer.java
@@ -9,38 +9,44 @@ public class CPlayer {
     private int id;
     private ArrayList<CDomino> hand;
     
-    
+    //player constructor
     public CPlayer (int idNum)
     {
         id = idNum;
         hand = new ArrayList();
     }
     
+    //returns hand
     public ArrayList getHand()
     {
         return hand;
     }
     
+    //returns player ID
     public int getId()
     {
         return id;
     }
     
+    //adds piece to player's hand
     public void addPiece(CDomino domino)
     {
         hand.add(domino);
     }
     
+    //removes piece from player's hand
     public void removePiece(CDomino domino)
     {
         hand.remove(domino);
     }
     
+    //checks if hand is empty
     public boolean isEmpty()
     {
         return hand.isEmpty();
     }
     
+    //determines if the player has a possible play
     public boolean hasPlay(int front, int end)
     {
     	if (hand != null) {
@@ -57,6 +63,7 @@ public class CPlayer {
         return false;
     }
     
+    //prints possible plays for the player
     public void showPlays(int front, int end) {
     	if (hand != null) {
     		for (int i = 0; i < hand.size(); i++) {
@@ -73,6 +80,7 @@ public class CPlayer {
     	}
     }
     
+    //prints hand for the player
     public void showHand() {
     	if (hand != null) {
     		System.out.println("Here is your hand");
